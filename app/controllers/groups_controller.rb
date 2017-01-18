f7a8d8b087382b7a8f4a5c53b07beee5e7d3099a
simon <wangzd.fudan@gmail.com>
2017-01-19 01:15:48 +0800

    check the group owner first

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create,]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy, ]
   def index
     @groups = Group.all
   end
@@ -11,7 +11,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     @group.user = current_user
-    
+
     if @group.save
     redirect_to groups_path
     flash[:notice] = "create"
@@ -26,6 +26,9 @@ class GroupsController < ApplicationController
 
   def edit
     @group = Group.find(params[:id])
+    if current_user != @group.user
+      redirect_to root_path, alert:"you have no permission"
+    end
   end
 
   def update
