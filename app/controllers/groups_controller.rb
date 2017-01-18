6ab470e4c6063bf189141d319f686c362be30df1
simon <wangzd.fudan@gmail.com>
2017-01-18 21:06:53 +0800

    implement new

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,10 +2,23 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+  def new
+    @group = Group.new
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.save
+    redirect_to groups_path
+    flash[:notice] = "create"
+  end
 
 
 
 
 
-  
 end
